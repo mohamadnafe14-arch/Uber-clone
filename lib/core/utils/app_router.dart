e4dbf7c2252de1aab92dde43c222120350b6aaf3
Mohamed <mohamadnafe14@gmail.com>
2026-07-16 11:11:29 +0300

    adding sign in view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:go_router/go_router.dart';
+import 'package:uber_clone/features/auth/presentation/views/sign_in_view.dart';
 import 'package:uber_clone/features/auth/presentation/views/sign_up_view.dart';
 
 class AppRouter {
   static const String initialRoute = '/';
+  static const String signInRoute = '/sign-in';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: initialRoute,
         builder: (context, state) => const SignUpView(),
+      ),
+      GoRoute(
+        path: signInRoute,
+        builder: (context, state) => const SignInView(),
       ),
     ],
   );
